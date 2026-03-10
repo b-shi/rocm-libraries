b1d58a52aec1d51da2762bf8cd7884717ec3b68a
b-shi <brianshi@amd.com>
2026-03-10 14:01:57 -0500

    Add loop and ptr update code

--- projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
+++ projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
@@ -4234,6 +4234,7 @@ class KernelWriter(metaclass=abc.ABCMeta):
       # SrdD/SrdC are used starting now, remove from sgpr pool
       self.removeSgprVarFromPool("SrdD")
       self.removeSgprVarFromPool("SrdC")
+      self.removeSgprVarFromPool("SrdWS")
       module.add(self.globalWriteWorkGroupInit(kernel))
       #if self.states.doShadowInit == 2:
       #  module.add(self.initC(kernel)) # initC while waiting for global reads
@@ -4280,7 +4281,7 @@ class KernelWriter(metaclass=abc.ABCMeta):
     module.add(lraTileAssignmentScaleSwizzled(self, kernel))
 
 
-
+    module.add(self.calculateLoopNumIter(kernel, tensorParametersA, tensorParametersB, self.states.unrollIdx))
 
 
 
@@ -4344,6 +4345,7 @@ class KernelWriter(metaclass=abc.ABCMeta):
       if not self.states.doShadowInit:
         self.removeSgprVarFromPool("SrdD")
         self.removeSgprVarFromPool("SrdC")
+        self.removeSgprVarFromPool("SrdWS")
         module.add(self.globalWriteWorkGroupInit(kernel))
 
       ####################################
