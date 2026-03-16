46038cd1ea4347dc63c2744e378a65d36cd8187c
Seb Vince <sebvince@amd.com>
2026-03-16 13:26:15 -0500

    LDS padding

--- projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
+++ projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
@@ -4950,8 +4950,15 @@ class KernelWriter(metaclass=abc.ABCMeta):
 
       self.ldsStartOffsetA = 0
       aTileInfo = self.states.a.tileInfo
+      bTileInfo = self.states.a.tileInfo
       numASubtiles = aTileInfo.globalSubtileGrid[0] * aTileInfo.globalSubtileGrid[1]
-      self.ldsStartOffsetB = numASubtiles * aTileInfo.subtileSize
+      numBSubtiles = bTileInfo.globalSubtileGrid[0] * bTileInfo.globalSubtileGrid[1]
+      readSize = 2*aTileInfo.subtileSize
+      # Align A and B sizes to readSize for DTL 2xsubtile reads
+      sizeA = ((numASubtiles * aTileInfo.subtileSize + readSize-1) // readSize) * readSize
+      sizeB = ((numBSubtiles * bTileInfo.subtileSize + readSize-1) // readSize) * readSize
+      self.ldsStartOffsetB = sizeA
+      kernel["LdsNumBytes"] = (sizeA + sizeB) * kernel["NumLdsBlk"]
       
 
     #print(self.states.a.tileInfo.getLocalSubtileId(1,0))
