74139e806c12a955ae05a3cdddbdc6a9084e3f80
Seb Vince <sebvince@amd.com>
2026-04-08 04:13:06 -0500

    Fix regression on PGR0

--- projects/hipblaslt/tensilelite/Tensile/Components/SubtileBasedKernel.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/SubtileBasedKernel.py
@@ -1274,8 +1274,11 @@ def emitSingleBufferLoad(tileInfo, kernel, sId0, sId1):
 
   # When loadRatioGR > 1, multiple subtiles share one global read.
   # Only emit the load for the first subtile of each group.
-  if sId0 != int(grBaseId * tileInfo.loadRatioGR):
-    return module
+  if tileInfo.loadRatioGR > 1:
+    linearId = tileInfo.getLocalSubtileLinearId(sId0, sId1)
+    firstInGroup = int(grBaseId * tileInfo.loadRatioGR)
+    if linearId != firstInGroup:
+      return module
 
   tc = tileInfo.tc
   isGlc = bool(kernel["NonTemporal%s"%tc] & 0x1)
