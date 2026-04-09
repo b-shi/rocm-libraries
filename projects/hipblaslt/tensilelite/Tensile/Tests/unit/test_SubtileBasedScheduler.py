71b17bd631c16e42cac478cf67fea7fe34754d7f
Seb Vince <sebvince@amd.com>
2026-04-09 08:39:43 -0500

    Update print functions

--- projects/hipblaslt/tensilelite/Tensile/Tests/unit/test_SubtileBasedScheduler.py
+++ projects/hipblaslt/tensilelite/Tensile/Tests/unit/test_SubtileBasedScheduler.py
@@ -735,7 +735,7 @@ if __name__ == "__main__":
     print("=== DEFAULT ===")
     s.printSchedule()
     print("\n=== VGPR + DEPS ===")
-    s.printSchedule(showVgpr=False, showDeps=True, showSubtiles=False)
+    s.printSchedule(showVgpr=True, showDeps=True, showSubtiles=False)
     s.printSchedule()
 
     s.allocVgprTiles(writer)
